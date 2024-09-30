aa2c7d5ede8f93be96e183f173d35593de6f91f9
Shanu-Ch-Ghosh <shanughosh236@gmail.com>
2024-09-30 17:31:08 +0530

    fsf

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-        <ol key="relativeList">
-          <li key="relativeListItem1">Apple</li>
-          <li key="relativeListItem2">Orange</li>
+        <ol key="#relativeList">
+          <li key="#relativeListItem1">Shanu</li>
+          <li key="#relativeListItem2">Samir</li>
+          <li key="#relativeListItem3">Ram</li>
+          <li key="#relativeListItem4">Shyam</li>
+          <li key="#relativeListItem5">Madhu</li>
+          <li key="#relativeListItem6">Karan</li>
         </ol>
     </div>
   )
