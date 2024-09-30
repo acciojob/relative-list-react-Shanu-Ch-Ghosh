750e8e637acc644ebcaaad3fc22de4db79ffb32f
Shanu-Ch-Ghosh <shanughosh236@gmail.com>
2024-09-30 17:24:11 +0530

    kj

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+        <ol key="relativeList">
+          <li key="relativeListItem1">Apple</li>
+          <li key="relativeListItem2">Orange</li>
+        </ol>
     </div>
   )
 }
